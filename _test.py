b9491c18c3663a6bca7ca4d2a2038e2ed5021a09
Nick Murphy <nick@protocol46.com>
2021-01-28 14:57:12 -0600

    Added a deconvert abstract method and a roman numeral deconvert to match Moved the main functionality into the if main section to allow for coverage

--- _test.py
+++ _test.py
@@ -15,59 +15,70 @@ def test_roman_str_input():
     """
     Makes sure string inputs work
     """
-    assert Roman("3594").result == "MMMDXCIV"
+    converter = Roman("3594")
+    converter.convert()
+    assert converter.result == "MMMDXCIV"
 
 def test_roman_int_input():
     """
     Makes sure integer inputs work
     """
-    assert Roman(3594).result == "MMMDXCIV"
+    converter = Roman(3594)
+    converter.convert()
+    assert converter.result == "MMMDXCIV"
 
 def test_roman_raises_exception_on_non_int_values():
     """
     Makes sure inputs that are not integer values raises an error
     """
     with pytest.raises(ValueError):
-        Roman("g").result
+        converter = Roman("g")
+        converter.convert()
 
 def test_roman_raises_exception_on_wrong_low_int_values():
     """
     Makes sure inputs that are too low integer values raises an error
     """
     with pytest.raises(ValueError):
-        Roman("0").result
+        converter = Roman("0")
+        converter.convert()
 
 def test_roman_raises_exception_on_wrong_high_int_values():
     """
     Makes sure inputs that are too high integer values raises an error
     """
     with pytest.raises(ValueError):
-        Roman("4000").result 
+        converter = Roman("4000")
+        converter.convert()
 
 def test_roman_succeeds_on_correct_values_ones():
     """
     Makes sure inputs that are within the requirements range succeed
     """
     for test_val in range(1, 10):
-        Roman(test_val).result
+        converter = Roman(test_val)
+        converter.convert()
 
 def test_roman_succeeds_on_correct_values_tens():
     """
     Makes sure inputs that are within the requirements range succeed
     """
     for test_val in range(10, 100):
-        Roman(test_val).result
+        converter = Roman(test_val)
+        converter.convert()
 
 def test_roman_succeeds_on_correct_values_hundreds():
     """
     Makes sure inputs that are within the requirements range succeed
     """
     for test_val in range(100, 1000):
-        Roman(test_val).result
+        converter = Roman(test_val)
+        converter.convert()
 
 def test_roman_succeeds_on_correct_values_thousands():
     """
     Makes sure inputs that are within the requirements range succeed
     """
     for test_val in range(1000, 3999):
-        Roman(test_val).result
+        converter = Roman(test_val)
+        converter.convert()
--- converter.py
+++ converter.py
@@ -24,11 +24,12 @@ class Converter(ABC):
 
     Abstract class object for a baseline Converter.
     """
-    def __init__(self, number):
+    
+    def __init__(self, value):
         super(Converter, self).__init__()
-        # Inputted number to be converted
-        self.number = number
-        # Result value
+        # Inputted value to be converted
+        self.value = value
+        # Resulting converted value
         self.result = None
 
     @abstractmethod
@@ -37,7 +38,16 @@ class Converter(ABC):
         convert
         ~~~~~~~~~~
 
-        Abstract method for new subclasses to overide for their own conversion.
+        Abstract convert method for new subclasses to overide for their own conversion.
+        """
+    
+    @abstractmethod
+    def deconvert(self):
+        """
+        deconvert
+        ~~~~~~~~~~
+
+        Abstract deconvert method for new subclasses to overide for their own deconversion.
         """
 
 
@@ -48,10 +58,17 @@ class Roman(Converter):
 
     A roman numeral converter.
     """
-    def __init__(self, number):
-        super(Roman, self).__init__(number)
-        # Run the conversion
-        self.convert()
+
+    def __init__(self, value):
+        super(Roman, self).__init__(value)
+        # Roman numerals with their respective value
+        self.roman_numeral_table = [
+                ("M", 1000), ("CM", 900), ("D", 500),
+                ("CD", 400), ("C", 100), ("XC", 90),
+                ("L", 50), ("XL", 40), ("X", 10),
+                ("IX", 9), ("V", 5), ("IV", 4),
+                ("I", 1)
+            ]
 
     def convert(self):
         """
@@ -61,45 +78,93 @@ class Roman(Converter):
         Takes an integer and converts it to it's equivilant string
         of roman numerals, and sets the result.
         """
+
         # Make sure the user input is an integer between 0 and 4000
-        integer = int(self.number)
+        integer = int(self.value)
         if not 1 <= integer <= 3999:
             raise ValueError("Enter a valid integer between 0 and 4000")
 
-        # Symbol with it's respective integer place
-        roman_numeral_table = [
-            ("M", 1000), ("CM", 900), ("D", 500),
-            ("CD", 400), ("C", 100), ("XC", 90),
-            ("L", 50), ("XL", 40), ("X", 10),
-            ("IX", 9), ("V", 5), ("IV", 4),
-            ("I", 1)
-        ]
         # Initilize the numeral list
         roman_numerals = []
+
         # Exit when integer reaches 0
         while integer:
             # Loop over the roman numeral table
-            for numeral, value in roman_numeral_table:
+            for numeral, value in self.roman_numeral_table:
                 # Get the quotent and remainder from a %mod calculation
                 count, integer = divmod(integer, value)
                 # Add the resulting numerals to the final result list
                 roman_numerals.append(numeral * count)
+        
         # Join the final result (which is faster than str concatenation) into a str
         self.result = ''.join(roman_numerals)
 
+    def deconvert(self):
+        """
+        deconvert
+        ~~~~~~~~~~
 
-def main():
-    """
-    main
-    ~~~~~~~~~~
+        Takes a roman numeral str and deconverts it to it's equivilant
+        integer, and sets the result.
+        """
 
-    Example usage when this file is run directly
-    """
-    # Get user input with a prompt
-    user_input = input("Please input a integer between 0 and 3999: ")
-    # Show the results
-    print("Result is: ", Roman(user_input).result)
+        # Make sure the user input is a roman numeral between I and MMMCMXCIX
+        usr_str = str(self.value)
+        if not 1 <= len(usr_str) <= 9:
+            raise ValueError("Enter a valid roman numeral between I and MMMCMXCIX")
+        
+        # Internal result int to replace class result var
+        result = 0
+        # 
+        double_found = False
+
+        # Loop through each character in usr_str
+        for index, char in enumerate(usr_str):
+            # If a double character was found
+            if double_found == True:
+                # Reset the flag
+                double_found = False
+                # Skip this character (as it was part of the 'double')
+                continue
+
+            # Initilize/reset the holding integer
+            holding_val = 0
+
+            # Loop over the roman numeral table
+            for numeral, value in self.roman_numeral_table:
+                # Check for a two character roman numeral
+                if usr_str[index:index+2] == numeral:
+                    # Set holding_val to the found value  
+                    holding_val = value
+                    # Flag that a double character roman numeral was found
+                    double_found = True
+                    # Move on to the next character
+                    break
+
+                # Check if this current character matches this roman numeral
+                elif char == numeral:
+                    # Temporarely hold the resulting integer in case a two character is found
+                    holding_val = value
+
+            # Add the found value to the final result
+            result += holding_val
+
+        # Set the class var result to be the deconvert result MMMCMXCIX
+        self.result = result
 
 
 if __name__ == "__main__":
-    main()
+    # Get user input with a prompt
+    value = input("Please input a integer between 0 and 3999: ")
+
+    # Create a converter with input
+    converter = Roman(value)
+
+    # Convert the input
+    converter.convert()
+    print("Convert is: ", converter.result)
+
+    # Deconvert back 
+    converter.value = converter.result
+    converter.deconvert()
+    print("Deconvert is: ", converter.result)
